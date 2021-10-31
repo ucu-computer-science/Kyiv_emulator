713a755bc6a60d0b6c15926631fbda7bb42507e1
oliuba <olgaluba3@gmail.com>
2021-10-31 19:14:51 +0200

    Some attempts to start doing something useful

---
+++ processor.py
@@ -0,0 +1,5 @@
+"""Main class of Kyiv machine emulator that contains all its memory and functions."""
+
+class Processor:
+    def __init__(self):
+        pass
--- registers.py
+++ registers.py
@@ -3,8 +3,13 @@ from bitarray import bitarray
 class Registers:
     """Registers of Kyiv machine"""
     def __init__(self):
-        self.address_r = bitarray('0' * 10)
-        self.return_r = bitarray('0' * 11)
+        self.address_r = bitarray('0' * 10, endian='little')
+        self.address_r[-1] = 1
+        print(self.address_r.to01())
+        self.return_r = bitarray('0' * 11, endian='little')
         self.cycle_r = bitarray('0' * 11)
         self.com_counter = bitarray('0' * 11)
         self.command_r = bitarray('0' * 41)
+
+
+r = Registers()
